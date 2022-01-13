6a5812ab1214188ca4cff906bc3e807779956106
hysunflower <nothing_upc@163.com>
2022-01-13 19:00:15 +0800

    update resnet

--- ResNet50V1.5/scripts/clas_fp32.sh
+++ ResNet50V1.5/scripts/clas_fp32.sh
@@ -3,6 +3,11 @@
 base_batch_size=$1
 num_gpus=$2
 
+export FLAGS_eager_delete_tensor_gb=0.0
+export FLAGS_fraction_of_gpu_memory_to_use=0.98
+export FLAGS_cudnn_exhaustive_search=1
+export FLAGS_conv_workspace_size_limit=4000
+
 train_cmd="-c ppcls/configs/ImageNet/ResNet/ResNet50.yaml
            -o Global.print_batch_step=10
            -o DataLoader.Train.sampler.batch_size=${base_batch_size}
@@ -14,7 +19,7 @@ train_cmd="-c ppcls/configs/ImageNet/ResNet/ResNet50.yaml
            -o DataLoader.Train.loader.num_workers=8
            -o Global.epochs=1"
 
-if [ ${num_gpus} == 1 ]; then
+if [[ ${num_gpus} == 1 ]]; then
     train_cmd="python -u ppcls/static/train.py "${train_cmd}
 else
      train_cmd="python -m paddle.distributed.launch --log_dir=./mylog --gpus=0,1,2,3,4,5,6,7 ppcls/static/train.py "${train_cmd}" -o Global.use_dali=True"
